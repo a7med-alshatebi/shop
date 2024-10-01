5259a5791ab56bcff808589280f65eb489e77f4d
a7med-alshatebi <ahmedalshatibi22@gmail.com>
2024-10-02 01:30:39 +0300

    sadadadada

--- script.js
+++ script.js
@@ -28,6 +28,6 @@ function sendMail(){
         message: document.getElementById('message').value,
 
     }
-    emailjs.send('service_q2lb6te','template_i03meq7',parms).then(alert('Email Sent Successfully'));
+    emailjs.send("service_q2lb6te","template_8jbfrkf");
 }
 
