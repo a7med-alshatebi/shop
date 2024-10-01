64885b69b5c27db5c965b13dbf35839709557ef2
a7med-alshatebi <ahmedalshatibi22@gmail.com>
2024-10-02 02:22:12 +0300

    email alert added

---
+++ msgalert.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let parms = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value,
+
+    }
+    emailjs.send("service_q2lb6te","template_i03meq7",parms).then(alert('Email Sent Successfully'));
+}
--- script.js
+++ script.js
@@ -18,16 +18,7 @@ if (close){
 }
 
 
-////////////////emailJS/////////////
 
-function sendMail(){
-    let parms = {
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        subject: document.getElementById('subject').value,
-        message: document.getElementById('message').value,
 
-    }
-    emailjs.send("service_q2lb6te","template_i03meq7",parms).then(alert('Email Sent Successfully'));
-}
+
 
